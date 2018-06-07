b8cd74426a06dc92a999c9a318bd3fa1bf6b13bf
manzor99 <manzor99@gmail.com>
2018-06-06 22:22:22 -0500

    made a frame. Not totally sure if I want to make my own canvas class or not yet, but for now this is the basic setup.

--- src/game/Game.java
+++ src/game/Game.java
@@ -1,5 +1,9 @@
 package game;
 
 public class Game{
-
+	GameFrame frame;
+	public Game() {
+		
+	}
+	
 }
--- src/game/GameFrame.java
+++ src/game/GameFrame.java
@@ -1,7 +1,27 @@
 package game;
 
 import javax.swing.JFrame;
+import java.awt.Canvas;
+import java.awt.Color;
 
 public class GameFrame extends JFrame{
-
+	Canvas canvas;
+	public GameFrame(int width, int height) {
+		super();
+		
+		canvas = new Canvas();
+		canvas.setSize(width, height);
+		canvas.setBackground(Color.BLUE);
+		
+		this.setSize(width, height);
+		this.setResizable(false);
+		this.add(canvas);
+		this.setTitle("2D-Rainbow6-Siege");
+		this.setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		GameFrame frame = new GameFrame(900, 600);
+	}
+	
 }
