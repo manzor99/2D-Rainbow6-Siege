e6be1069a9592b1e34c6ecd2af8eb033ec4c42f6
Larry <aznninja64@yahoo.com>
2018-06-07 22:35:30 -0500

    added constructor into R4C and added bullet class

--- src/guns/R4C.java
+++ src/guns/R4C.java
@@ -1,11 +1,14 @@
 
 public class R4C extends Assault
 {
+	public R4C()
+	{
 	int magsize = 30;
 	int RPM = 860;
+	}
 	//Dmg - 39(0-25m) 25(35+m)
 	//Rpm - 860
-	//Mag - 30 + 1
+	//Mag - 30
 	public void fire() 
 	{
 		//Detects fire type
@@ -16,4 +19,6 @@ public class R4C extends Assault
 	{
 		//take magsize - bulletsinmag from reserve. 
 	}
+
+	
 }
