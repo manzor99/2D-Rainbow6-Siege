7dab54ef4f2e10be8a17d7e14c972ab4b8252254
manzor99 <manzor99@gmail.com>
2018-06-08 20:07:06 -0500

    made movement occur in the game class not the client class

--- src/game/Team.java
+++ src/game/Team.java
@@ -1,8 +1,15 @@
 package game;
 
+import characters.Character;
 import java.util.ArrayList;
 
 public class Team {
 	ArrayList<Character> players; //players on the team
 	int score; //number of rounds won
+	
+	public Team() {
+		players = new ArrayList<>();
+		score = 0;
+	}
+	
 }
